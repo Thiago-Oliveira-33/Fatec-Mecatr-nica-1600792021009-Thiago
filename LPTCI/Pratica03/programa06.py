54f43b3e04434178def439a1c1b45ec2d8800cb4
Thiago-Oliveira-33 <70581525+Thiago-Oliveira-33@users.noreply.github.com>
2020-09-09 12:48:26 -0300

    Create programa06.py

---
+++ LPTCI/Pratica03/programa06.py
@@ -0,0 +1,20 @@
+print('Olá!')
+nome = input('Qual seu nome?')
+print('Bom dia', nome)
+print('Vamus somar !')
+#Pede para o usuario dois numeros e realiza a soma deles
+numero1 = int(input('Informe um valor:'))
+numero2 = int(input('Informe outro valor:'))
+resultado = numero1 + numero2
+print('Resultado da soma:', resultado)
+print('correto.')
+correto = input('sim ou não?')
+print('Obrigado.')
+#Pede um valor em cm e converte ele para metros
+valor_cm = float(input('Informe um valor em cm:'))
+valor_m = valor_cm / 100
+print('Valor convertido em metros:', valor_m)
+#Exercicio 1
+temp_celsius = float(input('C:'))
+temp_kelvin = temp_celsius + 273
+print('O valor de ', temp_celsius, 'em Kelvin eh:', temp_kelvin)
